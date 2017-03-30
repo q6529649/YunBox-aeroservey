bbf372047a3668f29d6ca894b54a83002b3dd6e2
q6529649 <234812835@qq.com>
2017-03-30 18:03:42 +0800

    3/30 18 03

--- category.php
+++ category.php
@@ -11,39 +11,36 @@
 		</div>
 	</div>	
 </div>
-<div class="container">	
+<div class="container" style="width:1500px;">	
 	<div class="row kadima_blog_wrapper">
-		<div class="col-md-12">
+		<div class="col-md-12" >
 		<?php 
 		if ( have_posts()): 
 			while ( have_posts() ): the_post();?>
-				<div id="post-<?php the_ID(); ?>" <?php post_class('kadima_blog_full'); ?>>
-					<div class="col-md-3">
-						<ul class="blog-date-left">
-							<li class="kadima_post_date">
-							<?php if ( ('d M  y') == get_option( 'date_format' ) ) : ?>
-							<span class="date"><?php echo get_the_date('d'); ?></span><h6><?php echo get_the_date('M  y'); ?></h6>
-							<?php else : ?>
-							<span class="date"><?php echo get_the_date(); ?></span>
-							<?php endif; ?>
-							</li>
-						</ul>
-					</div>					
-					<div class="col-md-9 post-content-wrap">
+				<div class="col-md-4" id="post-<?php the_ID(); ?>" <?php post_class('kadima_blog_full'); ?>>
+			
+					<div class="col-md-12 post-content-wrap" style="margin-bottom:20px;">
+						<?php if(has_post_thumbnail()): 
+							$img = array('class' => 'kadima_img_responsive'); ?>
+							<div class="kadima_blog_thumb_wrapper_showcase">	
+								<a href="<?php the_permalink(); ?>">							
+								<div class="kadima_blog-img">
+								<?php the_post_thumbnail('blog_2c_thumb',$img); ?>						
+								</div>
+								</a>
+								<?php if (is_home()) : ?>
+								<div class="kadima_blog_thumb_wrapper_showcase_overlay">
+								<div class="kadima_blog_thumb_wrapper_showcase_overlay_inner ">
+									<div class="kadima_blog_thumb_wrapper_showcase_icons">
+										<a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>"><i class="fa fa-link"></i></a>
+									</div>
+								</div>
+								</div>
+								<?php endif; ?>
+							</div>
+						<?php endif; ?>
 						<?php if(has_post_thumbnail()): 
 						$img = array('class' => 'kadima_img_responsive'); ?>
-						<div class="kadima_blog_thumb_wrapper_showcase">						
-							
-							<?php if (is_home()) : ?>
-							<div class="kadima_blog_thumb_wrapper_showcase_overlay">
-							<div class="kadima_blog_thumb_wrapper_showcase_overlay_inner ">
-								<div class="kadima_blog_thumb_wrapper_showcase_icons">
-									<a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>"><i class="fa fa-link"></i></a>
-								</div>
-							</div>
-							</div>
-							<?php endif; ?>
-						</div>
 						<?php endif; ?>
 						<div class="kadima_fuul_blog_detail_padding">
 						<h2><?php if(!is_single()) {?><a href="<?php the_permalink(); ?>"><?php } ?><?php the_title(); ?></a></h2>
@@ -65,7 +62,17 @@
 							  );
 							  wp_link_pages( $defaults ); ?>
 						</div>
+						<ul class="blog-date-left">
+							<li class="kadima_post_date">
+							<?php if ( ('d M  y') == get_option( 'date_format' ) ) : ?>
+							<span class="date"><?php echo get_the_date('d'); ?></span><h6><?php echo get_the_date('M  y'); ?></h6>
+							<?php else : ?>
+							<span class="date"><?php echo get_the_date(); ?></span>
+							<?php endif; ?>
+							</li>
+						</ul>
 					</div>
+					
 				</div>
 		<?php
 			endwhile; 
